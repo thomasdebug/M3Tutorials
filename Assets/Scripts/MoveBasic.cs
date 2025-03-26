b8db70a661cf4cf3df2e87716f4f3356b78b3618
thomasdebug <38764@ma-web.nl>
2025-03-26 12:29:56 +0100

    tutorial 3

--- Assets/Scripts/MoveBasic.cs
+++ Assets/Scripts/MoveBasic.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
 public class MoveBasic : MonoBehaviour
 {
+    public Vector3 jump;
     public float jumpForce = 2.0f;
     private float speed = 5.0f; // Set player's movement speed.
     public Rigidbody rb; // Reference to player's Rigidbody.
+    public bool onFloor;
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody>(); // Access player's Rigidbody.
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "floor")
+        {
+            onFloor = true;
+        }
     }
 
     void Update()
@@ -34,6 +45,11 @@ public class MoveBasic : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
         {
             rb.position += Vector3.back * speed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && onFloor == true)
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            onFloor = false;
         }
     }
 }
